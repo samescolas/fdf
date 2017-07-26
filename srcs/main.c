7f97e85a9a093a73ca28395fdc3239d11b424ec8
samescolas <samescolas@gmail.com>
2017-07-26 16:48:29 -0700

    Drew a pixel

--- srcs/main.c
+++ srcs/main.c
@@ -7,6 +7,7 @@ int		main(void)
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 400, 400, "something");
-	//mlx_pixel_put(mlx, win, 
+	mlx_pixel_put(mlx, win, 200, 200, 0x00FFFFFF);
+	mlx_loop(mlx);
 	return (0);
 }
