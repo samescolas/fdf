034a5d57463579ca2f1e1cd48a0c55bbf159b399
samescolas <samescolas@gmail.com>
2017-07-26 16:42:52 -0700

    Adding test code

---
+++ srcs/main.c
@@ -0,0 +1,12 @@
+#include "../libs/minilibx/mlx.h"
+
+int		main(void)
+{
+	void	*mlx;
+	void	*win;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 400, 400, "something");
+	//mlx_pixel_put(mlx, win, 
+	return (0);
+}
