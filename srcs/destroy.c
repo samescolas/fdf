f13c0ae52c5abb1cc745c1e734c9ccbaa305aa7f
samescolas <samescolas@gmail.com>
2017-07-29 09:46:18 -0700

    Adding function to destroy environment upon exit.

---
+++ srcs/destroy.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destroy.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/29 09:10:35 by sescolas          #+#    #+#             */
+/*   Updated: 2017/07/29 09:45:55 by sescolas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libs/libft/libft.h"
+#include "../includes/fdf.h"
+
+static void	fdf_destroy_window(t_window *window)
+{
+	window->mlx = (void *)0;
+	window->window = (void *)0;
+	window->height = 0;
+	window->width = 0;
+	free(window);
+	window = (void *)0;
+}
+
+static void	fdf_destroy_blueprint(
+		unsigned short **blueprint,
+		unsigned short rows,
+		unsigned short cols)
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i < rows)
+	{
+		j = -1;
+		while (++j < cols)
+		{
+			blueprint[i][j] = 0;
+		}
+		free(blueprint[i]);
+		blueprint[i] = (void *)0;
+	}
+	free(blueprint);
+	blueprint = (void *)0;
+}
+
+static void	fdf_destroy(t_fdf *fdf)
+{
+	fdf_destroy_window(fdf->window);
+	fdf_destroy_blueprint(fdf->blueprint, fdf->bp_rows, fdf->bp_cols);
+	free(fdf->keys);
+	fdf->bp_rows = 0;
+	fdf->bp_cols = 0;
+	free(fdf);
+	fdf = (void *)0;
+}
+
+void		fdf_destroy_later(t_fdf *fdf, int loading)
+{
+	static t_fdf	*fdf_store;
+
+	if (loading)
+	{
+		if (fdf_store == (void *)0)
+			fdf_store = fdf;
+	}
+	else
+	{
+		if (fdf_store != (void *)0)
+			fdf_destroy(fdf_store);
+	}
+}
