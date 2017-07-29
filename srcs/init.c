f1271e2fb7185f70ac01b250c28bf46daade04ee
samescolas <samescolas@gmail.com>
2017-07-29 11:05:06 -0700

    Uncommented lines to display window.

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 17:48:22 by sescolas          #+#    #+#             */
-/*   Updated: 2017/07/29 09:44:40 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/07/29 10:00:09 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,9 @@ t_fdf	*fdf_init(
 			free(ret);
 			ft_fatal("err: out of memory\n");
 		}
-		//ret->window->mlx = mlx_init();
-		if (width && !height)
-			ret->window->window =
-				mlx_new_window(ret->window->mlx, width, height, title);
+		ret->window->mlx = mlx_init();
+		ret->window->window =
+			mlx_new_window(ret->window->mlx, width, height, title);
 		ret->window->height = height;
 		if (!(ret->blueprint = read_blueprint(filepath, &ret->bp_rows, &ret->bp_cols)))
 		{
