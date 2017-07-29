d423a565128221c6bfbb8632bd21c4937a2f9e59
samescolas <samescolas@gmail.com>
2017-07-29 14:37:19 -0700

    Added a function to create a gradient between two colors.

--- srcs/draw_line.c
+++ srcs/draw_line.c
@@ -6,11 +6,12 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 17:55:40 by sescolas          #+#    #+#             */
-/*   Updated: 2017/07/28 17:55:43 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/07/29 14:33:42 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libs/minilibx/mlx.h"
+#include "../libs/libft/libft.h"
 #include "../includes/fdf.h"
 
 void	draw_vertical_line(void *mlx, void *win, int x, int y0, int y1)
@@ -22,6 +23,7 @@ void	draw_vertical_line(void *mlx, void *win, int x, int y0, int y1)
 		mlx_pixel_put(mlx, win, x, y, 0xFFFFFF);
 }
 
+/*
 void	draw_line(void *mlx, void *win, int x0, int x1, int y0, int y1)
 {
 	long long	color;
@@ -52,6 +54,72 @@ void	draw_line(void *mlx, void *win, int x0, int x1, int y0, int y1)
 		pixel_count += 1;
 		err = err + derr;
 		if (err >= 0.5)
+		{
+			y += 1;
+			err -= 1;
+		}
+	}
+}
+*/
+
+int		ft_gradient(t_color c1, t_color c2)
+{
+	t_color	*ret;
+
+	ret = int_to_col(col_to_int(c1));
+	if (c1.r - c2.r != 0)
+	{
+		if (c1.r > c2.r)
+			ret->r = (ret->r + 11) % 256;
+		else
+			ret->r = (ret->r - 11) % 256;
+	}
+	if (c1.g - c2.g != 0)
+	{
+		if (c1.g > c2.g)
+			ret->g = (ret->g + 11) % 256;
+		else
+			ret->g = (ret->g - 11) % 256;
+	}
+	if (c1.b - c2.b != 0)
+	{
+		if (c1.b > c2.b)
+			ret->b = (ret->b + 11) % 256;
+		else
+			ret->b = (ret->b - 11) % 256;
+	}
+	return (col_to_int(*ret));
+}
+
+void	draw_line(t_window *window, t_point a, t_point b)
+{
+	int		x;
+	int		y;
+	float	err;
+	float	derr;
+	int		color;
+	int		pixel_count;
+
+	if (a.pos->x == b.pos->x)
+	{
+		draw_vertical_line(window->mlx, window->win, a.pos->x, a.pos->y, b.pos->y);
+		return ;
+	}
+	derr = (MAX(a.pos->y, b.pos->y) - MIN(a.pos->y, b.pos->y))/
+			(MAX(a.pos->x, b.pos->x) - MIN(a.pos->x, b.pos->x));
+	err = derr - 0.5;
+	y = MIN(a.pos->y, b.pos->y);
+	x = MIN(a.pos->x, b.pos->x) - 1;
+	color = col_to_int(*(a.col));
+	pixel_count = 0;
+	while (++x < MAX(a.pos->x, b.pos->x))
+	{
+		color  = ft_gradient(*(a.col), *(b.col));
+		a.col = int_to_col(ft_gradient((*a.col), *(b.col)));
+		mlx_pixel_put(window->mlx, window->win, x, y, color);
+		pixel_count += 1;
+		err = err + derr;
+		if (err > 0.5)
 		{
 			y += 1;
 			err -= 1;
