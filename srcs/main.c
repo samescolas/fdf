a7c8f2d8b1a39c102cbc4215a4b912f8ca9fa014
samescolas <samescolas@gmail.com>
2017-07-30 12:53:41 -0700

    Resized screen'

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 10:38:48 by sescolas          #+#    #+#             */
-/*   Updated: 2017/07/30 12:27:30 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/07/30 12:52:47 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,34 @@ void	print_grid2(t_fdf *fdf)
 	t_point			*a;
 	t_point			*b;
 
-	scale = 10;
+	scale = 75;
 	i = -1;
 	while (++i < fdf->bp_rows)
 	{
 		j = -1;
 		while (++j < fdf->bp_cols - 1)
 		{
+			if (i + 1 < fdf->bp_rows && j + 2 < fdf->bp_cols)
+			{
+				a = create_point(
+						create_coord(j, i, fdf->blueprint[i][j]),
+						create_color(0, 0, 0),
+				  		scale);
+				b = create_point(
+						create_coord(j + 1, i + 1, fdf->blueprint[i + 1][j + 1]),
+						create_color(0, 255, 0),
+						scale);
+				draw_line(fdf->window, *a, *b);
+			}
 			if (i + 1 < fdf->bp_rows)
 			{
 				a = create_point(
 						create_coord(j, i, fdf->blueprint[i][j]),
-						create_color(255, 0, 0),
+						create_color(0, 0, 0),
 				  		scale);
 				b = create_point(
-						create_coord(j, i + 1, fdf->blueprint[i][j]),
-						create_color(0, 0, 0),
+						create_coord(j, i + 1, fdf->blueprint[i + 1][j]),
+						create_color(0, 255, 0),
 						scale);
 				draw_line(fdf->window, *a, *b);
 			}
@@ -47,11 +59,11 @@ void	print_grid2(t_fdf *fdf)
 			{
 				a = create_point(
 						create_coord(j, i, fdf->blueprint[i][j]),
-						create_color(255, 0, 0),
+						create_color(0, 0, 0),
 						scale);
 				b = create_point(
-						create_coord(j + 1, i, fdf->blueprint[i][j]),
-						create_color(0, 0, 0),
+						create_coord(j + 1, i, fdf->blueprint[i][j + 1]),
+						create_color(0, 255, 0),
 						scale);
 				draw_line(fdf->window, *a, *b);
 			}
@@ -87,7 +99,7 @@ int		main(int argc, char **argv)
 
 	if (argc == 2)
 	{
-		if (!(fdf = fdf_init(400, 400, "Testing...", argv[1])))
+		if (!(fdf = fdf_init(1200, 800, "Testing...", argv[1])))
 			return (0);
 		print_grid2(fdf);
 
