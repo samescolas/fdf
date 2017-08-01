fce14a33551977bf516ec9b79b656d742f6b70fe
samescolas <samescolas@gmail.com>
2017-08-01 10:54:34 -0700

    Reworked to use to float arrays rather than unneccesary structs.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 10:38:48 by sescolas          #+#    #+#             */
-/*   Updated: 2017/07/31 19:30:06 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/08/01 10:52:45 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	plot_grid(t_fdf *fdf)
 	int				i;
 	int				j;
 	int				scale;
-	t_point			*a;
-	t_point			*b;
+	float			v1[3];
+	float			v2[3];
 
-	scale = 75;
+	scale = 25;
 	i = -1;
 	while (++i < fdf->bp_rows)
 	{
@@ -33,7 +33,14 @@ void	plot_grid(t_fdf *fdf)
 		{
 			if (i + 1 < fdf->bp_rows && j + 2 < fdf->bp_cols)
 			{
-				a = create_point(
+				v1[0] = j * scale;
+				v1[1] = i * scale;
+				v1[2] = fdf->blueprint[i][j];
+				v2[0] = (j + 1) * scale;
+				v2[1] = (i + 1) * scale;
+				v2[2] = fdf->blueprint[i + 1][j + 1];
+				draw_line(fdf->window, v1, v2);
+				/*a = create_point(
 						create_coord(j, i, fdf->blueprint[i][j]),
 						create_color(0, 255, 0),
 				  		scale);
@@ -42,9 +49,18 @@ void	plot_grid(t_fdf *fdf)
 						create_color(0, 255, 0),
 						scale);
 				draw_line(fdf->window, *a, *b);
+				*/
 			}
 			if (i + 1 < fdf->bp_rows)
 			{
+				v1[0] = j * scale;
+				v1[1] = i * scale;
+				v1[2] = fdf->blueprint[i][j];
+				v2[0] = j * scale;
+				v2[1] = (i + 1) * scale ;
+				v2[2] = fdf->blueprint[i + 1][j];
+				draw_line(fdf->window, v1, v2);
+				/*
 				a = create_point(
 						create_coord(j, i, fdf->blueprint[i][j]),
 						create_color(0, 255, 0),
@@ -54,9 +70,18 @@ void	plot_grid(t_fdf *fdf)
 						create_color(0, 255, 0),
 						scale);
 				draw_line(fdf->window, *a, *b);
+				*/
 			}
 			if (j + 2 < fdf->bp_cols)
 			{
+				v1[0] = j * scale;
+				v1[1] = i * scale;
+				v1[2] = fdf->blueprint[i][j];
+				v2[0] = (j + 1) * scale;
+				v2[1] = i * scale;
+				v2[2] = fdf->blueprint[i][j + 1];
+				draw_line(fdf->window, v1, v2);
+				/*
 				a = create_point(
 						create_coord(j, i, fdf->blueprint[i][j]),
 						create_color(0, 255, 0),
@@ -66,6 +91,7 @@ void	plot_grid(t_fdf *fdf)
 						create_color(0, 255, 0),
 						scale);
 				draw_line(fdf->window, *a, *b);
+				*/
 			}
 		}
 	}
