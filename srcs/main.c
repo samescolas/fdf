42c0465a929f49fb9f155d447feee05286def6ac
samescolas <samescolas@gmail.com>
2017-08-02 15:01:18 -0700

    Plot grid is still hanging out in here.

--- srcs/main.c
+++ srcs/main.c
@@ -6,92 +6,90 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 10:38:48 by sescolas          #+#    #+#             */
-/*   Updated: 2017/08/01 10:52:45 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/08/02 14:56:12 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "../libs/minilibx/mlx.h"
 #include "../libs/libft/libft.h"
 #include "../includes/fdf.h"
 #include "../includes/ft_atexit.h"
 
+void	model_to_world(t_fdf fdf, float v1[3], float v2[3])
+{
+	v1[0] = v1[0] * fdf.scale->x + fdf.translation->x;
+	v1[1] = v1[1] * fdf.scale->y + fdf.translation->y;
+	v1[2] = v1[2] * fdf.scale->z + fdf.translation->z;
+	v2[0] = v2[0] * fdf.scale->x + fdf.translation->x;
+	v2[1] = v2[1] * fdf.scale->y + fdf.translation->y;
+	v2[2] = v2[2] * fdf.scale->z + fdf.translation->z;
+}
+
+void	world_to_view(t_fdf fdf, float v1[3], float v2[3])
+{
+	float	scale;
+
+	scale = tan(fdf.fov / 2 * M_PI / 180) * fdf.near;
+
+	//v1[0] = (2 * v1[0] * fdf.near) / (2 * fdf.far);
+	//v1[1] = (2 * v1[1] * fdf.near) / (2 * fdf.far);
+
+	//v2[0] = (2 * v2[0] * fdf.near) / (2 * fdf.far);
+	//v2[1] = (2 * v2[1] * fdf.near) / (2 * fdf.far);
+
+	v1[0] = v1[0] / (v1[2] == 0 ? 1 : v1[2]);
+	v1[1] = v1[1] / (v1[2] == 0 ? 1 : v1[2]);
+
+	v2[0] = v2[0] / (v2[2] == 0 ? 1 : v2[2]);
+	v2[1] = v2[1] / (v2[2] == 0 ? 1 : v2[2]);
+}
+
+void	fill_vec(float v[3], float x, float y, float z)
+{
+	v[0] = x;
+	v[1] = y;
+	v[2] = z;
+}
+
 void	plot_grid(t_fdf *fdf)
 {
 	int				i;
 	int				j;
-	int				scale;
 	float			v1[3];
 	float			v2[3];
 
-	scale = 25;
 	i = -1;
 	while (++i < fdf->bp_rows)
 	{
 		j = -1;
 		while (++j < fdf->bp_cols - 1)
 		{
-			if (i + 1 < fdf->bp_rows && j + 2 < fdf->bp_cols)
+			if (0 && i + 1 < fdf->bp_rows && j + 2 < fdf->bp_cols)
 			{
-				v1[0] = j * scale;
-				v1[1] = i * scale;
-				v1[2] = fdf->blueprint[i][j];
-				v2[0] = (j + 1) * scale;
-				v2[1] = (i + 1) * scale;
-				v2[2] = fdf->blueprint[i + 1][j + 1];
+				fill_vec(v1, j, i, fdf->blueprint[i][j]);
+				fill_vec(v2, j + 1, i + 1, fdf->blueprint[i + 1][j + 1]);
+				model_to_world(*fdf, v1, v2);
+				//world_to_view(*fdf, v1, v2);
 				draw_line(fdf->window, v1, v2);
-				/*a = create_point(
-						create_coord(j, i, fdf->blueprint[i][j]),
-						create_color(0, 255, 0),
-				  		scale);
-				b = create_point(
-						create_coord(j + 1, i + 1, fdf->blueprint[i + 1][j + 1]),
-						create_color(0, 255, 0),
-						scale);
-				draw_line(fdf->window, *a, *b);
-				*/
 			}
 			if (i + 1 < fdf->bp_rows)
 			{
-				v1[0] = j * scale;
-				v1[1] = i * scale;
-				v1[2] = fdf->blueprint[i][j];
-				v2[0] = j * scale;
-				v2[1] = (i + 1) * scale ;
-				v2[2] = fdf->blueprint[i + 1][j];
+				fill_vec(v1, j, i, fdf->blueprint[i][j]);
+				fill_vec(v2, j, i + 1, fdf->blueprint[i + 1][j]);
+				model_to_world(*fdf, v1, v2);
+				//world_to_view(*fdf, v1, v2);
 				draw_line(fdf->window, v1, v2);
-				/*
-				a = create_point(
-						create_coord(j, i, fdf->blueprint[i][j]),
-						create_color(0, 255, 0),
-				  		scale);
-				b = create_point(
-						create_coord(j, i + 1, fdf->blueprint[i + 1][j]),
-						create_color(0, 255, 0),
-						scale);
-				draw_line(fdf->window, *a, *b);
-				*/
 			}
 			if (j + 2 < fdf->bp_cols)
 			{
-				v1[0] = j * scale;
-				v1[1] = i * scale;
-				v1[2] = fdf->blueprint[i][j];
-				v2[0] = (j + 1) * scale;
-				v2[1] = i * scale;
-				v2[2] = fdf->blueprint[i][j + 1];
+				fill_vec(v1, j, i, fdf->blueprint[i][j]);
+				fill_vec(v2, j + 1, i, fdf->blueprint[i][j + 1]);
+				model_to_world(*fdf, v1, v2);
+				//world_to_view(*fdf, v1, v2);
 				draw_line(fdf->window, v1, v2);
-				/*
-				a = create_point(
-						create_coord(j, i, fdf->blueprint[i][j]),
-						create_color(0, 255, 0),
-						scale);
-				b = create_point(
-						create_coord(j + 1, i, fdf->blueprint[i][j + 1]),
-						create_color(0, 255, 0),
-						scale);
-				draw_line(fdf->window, *a, *b);
-				*/
 			}
 		}
 	}
