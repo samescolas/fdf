21fc079e9c2e59e5555404c21a64741d539697d7
samescolas <samescolas@gmail.com>
2017-08-03 19:44:47 -0700

    Trying to figure out what's going wrong here.

--- srcs/display.c
+++ srcs/display.c
@@ -6,12 +6,49 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/03 08:43:40 by sescolas          #+#    #+#             */
-/*   Updated: 2017/08/03 16:13:33 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/08/03 19:10:42 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
+/*
+static void	rotate_xyz2(t_fdf fdf, float v1[3], float v2[3])
+{
+	t_coord a;
+
+	a = *fdf.rotation;
+	v1[0] = 
+}
+*/
+
+static void	rot_x(float a, float v1[3], float v2[3])
+{
+	v1[1] = v1[1] * cos(a) - v1[2] * sin(a);
+	v1[2] = v1[1] * sin(a) + v1[2] * cos(a);
+
+	v2[1] = v2[1] * cos(a) - v2[2] * sin(a);
+	v2[2] = v2[1] * sin(a) + v2[2] * cos(a);
+}
+
+static void	rot_y(float a, float v1[3], float v2[3])
+{
+	v1[0] = v1[0] * cos(a) + v1[2] * sin(a);
+	v1[2] = -1 * v1[0] * sin(a) + v1[2] * cos(a);
+
+	v2[0] = v2[0] * cos(a) + v2[2] * sin(a);
+	v2[2] = -1 * v2[0] * sin(a) + v2[2] * cos(a);
+}
+
+static void	rot_z(float a, float v1[3], float v2[3])
+{
+	v1[0] = v1[0] * cos(a) - v1[1] * sin(a);
+	v1[1] = v1[0] * sin(a) + v1[1] * cos(a);
+
+	v2[0] = v2[0] * cos(a) - v2[1] * sin(a);
+	v2[1] = v2[0] * sin(a) + v2[1] * cos(a);
+}
+/*
 static void	rotate_xyz(t_fdf fdf, float v1[3], float v2[3])
 {
 	t_coord	a;
@@ -37,17 +74,22 @@ static void	rotate_xyz(t_fdf fdf, float v1[3], float v2[3])
 		- v2[1] * (cos(a.x) * sin(a.y) * sin(a.z) + sin(a.x) * cos(a.z)) +
 		v2[2] * cos(a.x) * cos(a.y);
 }
-
+*/
 static void	normalize(t_fdf fdf, float v1[3], float v2[3])
 {
-	v1[0] = v1[0] - fdf.bp_cols / 2 + 1;
+	v1[0] = v1[0] - fdf.bp_cols / 2;
 	v1[1] = v1[1] - fdf.bp_rows / 2;
-	v2[0] = v2[0] - fdf.bp_cols / 2 + 1;
+	v2[0] = v2[0] - fdf.bp_cols / 2;
 	v2[1] = v2[1] - fdf.bp_rows / 2;
-	v1[2] = v1[2] * fdf.scale->z;
-	v2[2] = v2[2] * fdf.scale->z;
+//	v1[2] = v1[2] * fdf.scale->z;
+//	v2[2] = v2[2] * fdf.scale->z;
 
-	rotate_xyz(fdf, v1, v2);
+	//rotate_xyz(fdf, v1, v2);
+	
+	rot_x(fdf.rotation->x, v1, v2);
+	rot_y(fdf.rotation->y, v1, v2);
+	rot_z(fdf.rotation->z, v1, v2);
+
 
 	v1[0] = v1[0] + fdf.bp_cols / 2;
 	v1[1] = v1[1] + fdf.bp_rows / 2;
@@ -81,35 +123,35 @@ static void	fill_vec(float v[3], float x, float y, float z)
 
 void		plot_grid(t_fdf *fdf)
 {
-	int				i;
-	int				j;
+	int				row;
+	int				col;
 	float			v1[3];
 	float			v2[3];
 
-	i = -1;
-	while (++i < fdf->bp_rows)
+	row = -1;
+	while (++row < fdf->bp_rows)
 	{
-		j = -1;
-		while (++j < fdf->bp_cols - 1)
+		col = -1;
+		while (++col < fdf->bp_cols - 1)
 		{
-			if (0 && i + 1 < fdf->bp_rows && j + 2 < fdf->bp_cols)
+			if (0 && row + 1 < fdf->bp_rows && col + 2 < fdf->bp_cols)
 			{
-				fill_vec(v1, j, i, fdf->blueprint[i][j]);
-				fill_vec(v2, j + 1, i + 1, fdf->blueprint[i + 1][j + 1]);
+				fill_vec(v1, col, row, fdf->blueprint[row][col]);
+				fill_vec(v2, col + 1, row + 1, fdf->blueprint[row + 1][col + 1]);
 				normalize(*fdf, v1, v2);
 				draw_line(*fdf, v1, v2);
 			}
-			if (i + 1 < fdf->bp_rows)
+			if (row + 1 < fdf->bp_rows)
 			{
-				fill_vec(v1, j, i, fdf->blueprint[i][j]);
-				fill_vec(v2, j, i + 1, fdf->blueprint[i + 1][j]);
+				fill_vec(v1, col, row, fdf->blueprint[row][col]);
+				fill_vec(v2, col, row + 1, fdf->blueprint[row + 1][col]);
 				normalize(*fdf, v1, v2);
 				draw_line(*fdf, v1, v2);
 			}
-			if (j + 2 < fdf->bp_cols)
+			if (col + 2 < fdf->bp_cols)
 			{
-				fill_vec(v1, j, i, fdf->blueprint[i][j]);
-				fill_vec(v2, j + 1, i, fdf->blueprint[i][j + 1]);
+				fill_vec(v1, col, row, fdf->blueprint[row][col]);
+				fill_vec(v2, col + 1, row, fdf->blueprint[row][col + 1]);
 				normalize(*fdf, v1, v2);
 				draw_line(*fdf, v1, v2);
 			}
