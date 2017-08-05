7e479f31a02c9127d90e14bb9414a213ee0406a6
samescolas <samescolas@gmail.com>
2017-08-04 19:32:28 -0700

    Adding init.

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/03 09:49:05 by sescolas          #+#    #+#             */
-/*   Updated: 2017/08/04 11:06:52 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/08/04 19:19:42 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,24 @@
 #include "../libs/libft/libft.h"
 #include "../libs/minilibx/mlx.h"
 
-t_keys	*create_keylist(void)
+static void	assign_initial_position(t_fdf *fdf)
 {
-	t_keys	*ret;
-
-	if (!(ret = (t_keys *)malloc(sizeof(t_keys))))
-		ft_fatal("err: out of memory\n");
-	return (ret);
+	fdf->scale = create_coord(20, 20, 1);
+	fdf->translation = create_coord(600, 400, 0);
+	fdf->rotation = create_coord(0.33, -0.42, -0.22);
 }
 
-t_fdf	*fdf_init(short width, short height, char *title, char *filepath)
+void		assign_colors(t_fdf *fdf, int c1, int c2)
+{
+	t_color	*tmp;
+
+	tmp = int_to_col(c1);
+	fdf->colors[0] = *tmp;
+	tmp = int_to_col(c2);
+	fdf->colors[1] = *tmp;
+}
+
+t_fdf		*fdf_init(short width, short height, char *title, char *filepath)
 {
 	t_fdf	*ret;
 
@@ -35,20 +43,18 @@ t_fdf	*fdf_init(short width, short height, char *title, char *filepath)
 			ft_fatal("err: out of memory\n");
 		}
 		ret->window->mlx = mlx_init();
-		ret->window->win=
+		ret->window->win =
 			mlx_new_window(ret->window->mlx, width, height, title);
 		ret->window->height = height;
 		ret->window->width = width;
-		if (!(ret->blueprint =
-			read_blueprint(filepath, &ret->bp_rows, &ret->bp_cols, ret->z_minmax)))
+		if (!(ret->blueprint = read_blueprint(
+			filepath, &ret->bp_rows, &ret->bp_cols, ret->z_minmax)))
 		{
 			free(ret);
 			ft_fatal("err: out of memory\n");
 		}
-		ret->keys = create_keylist();
-		ret->scale = create_coord(20, 20, -0.25);
-		ret->translation = create_coord(150, 150, 0);
-		ret->rotation = create_coord(0, 0, 0);
+		assign_initial_position(ret);
+		assign_colors(ret, 4242, 174210);
 	}
 	fdf_destroy_later(ret, 1);
 	return (ret);
